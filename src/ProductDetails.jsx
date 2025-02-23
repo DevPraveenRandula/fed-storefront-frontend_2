dd6ea8d20c122fdd3ed8d506f4e6e823d84f931b
Praveen Randula <167463249+DevPraveenRandula@users.noreply.github.com>
2025-02-23 16:41:38 +0530

    Update ProductDetails.jsx

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -30,7 +30,7 @@ const ProductDetails = () => {
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/api/products/${id}`);
+        const response = await fetch(`https://fed-storefront-backend-praveen.onrender.com/api/products/${id}`);
         if (!response.ok) throw new Error("Failed to fetch product data");
         const data = await response.json();
         setProduct(data);
